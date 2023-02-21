9d9b3dfe2c381c4c8b0026ff5b9a467bb622712e
Proyecto3 <miperrogregorio@gmail.com>
2023-02-20 22:57:36 -0300

    funciona.001

---
+++ tasks/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpRequest
+from django.http import HttpResponse
+# Create your views here.
+def home (request):
+    return render(request, 'home.html')
+
+def signup(request):
+    
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password1']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Se a crado el Usuario')
+            except:  
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    "error": 'El Ususario ya Existe'   
+                })
+        return HttpResponse('Password no Coincide')    
